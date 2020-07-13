1199a1c19d90563b5e27b52752d9e5a1af2b3e6b
Eric Luo <eric.ys.luo@gmail.com>
2020-07-14 11:09:49 +1200

    Updates

---
+++ precache-manifest.11965ed53a2aa0871e4524ea04844dd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbfea82f34640cce4e603fcfa6a7874",
+    "url": "/tnba-app/index.html"
+  },
+  {
+    "revision": "40ba2e718c287b987ee8",
+    "url": "/tnba-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d84e53833d74d3cdcbe4",
+    "url": "/tnba-app/static/css/main.faf8e707.chunk.css"
+  },
+  {
+    "revision": "40ba2e718c287b987ee8",
+    "url": "/tnba-app/static/js/2.72d2071a.chunk.js"
+  },
+  {
+    "revision": "0e497545d211f6031a8a25aac134f7e9",
+    "url": "/tnba-app/static/js/2.72d2071a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84e53833d74d3cdcbe4",
+    "url": "/tnba-app/static/js/main.aa7b4556.chunk.js"
+  },
+  {
+    "revision": "7d837a282b60510e74e4",
+    "url": "/tnba-app/static/js/runtime-main.e166761a.js"
+  },
+  {
+    "revision": "b8cd74597dffd64d530c45b05b839009",
+    "url": "/tnba-app/static/media/logo.b8cd7459.png"
+  }
+]);
