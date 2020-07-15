c356fff3d7bb7eb9a37a269bcd7d533515a29a02
Eric Luo <eric.ys.luo@gmail.com>
2020-07-15 20:45:50 +1200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90e0c42332f5794d1913a056c9ed87a2.js"
+  "/precache-manifest.58c732eeb175253353dcb95a5002e3d9.js"
 );
 
 self.addEventListener('message', (event) => {
